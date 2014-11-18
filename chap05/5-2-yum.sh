c0a8afe80418df2ca024a5077bda8e1b87433f4f
KwangSeob Jeong <lesstif@gmail.com>
2014-11-18 18:32:11 +0900

    5 - 7장 추가

---
+++ chap05/5-2-yum.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+## mod_ssl 패키지 설치
+yum install mod_ssl
+
+## 이름이 httpd 로 시작하는 모든 패키지 설치
+yum install httpd*
+
+## httpd 패키지 삭제
+yum remove httpd
+
+## openssl 패키지 업데이트
+yum update openssl
+
+## 업데이트 가능한 패키지 확인
+yum check-update
+
+## 패키지와 메타 데이터 캐시 삭제
+yum clean packages metadata
+
+## 저장소 목록 보기
+yum repolist
+
+## 패키지 이름과 설명에 mysql 과 php 가 들어가는 패키지 검색
+yum search mysql php
+
+### httpd 로 시작하는 모든 패키지 목록 보기
+yum  list httpd*
+
+## 설치된 패키지 목록 보기
+yum list installed
+
+## httpd 패키지 정보 보기
+yum info httpd
+
+### httpd.conf 파일이 있는 패키지 검색
+yum provides '*/httpd.conf'
+
+### /etc/httpd* 경로 아래에 httpd.conf 파일이 있는 패키지 검색
+yum provides '/etc/httpd*/httpd.conf'
+
+
+## 로컬에 있는 rpm 패키지 설치
+yum localinstall mod_dav_svn* subversion* serf-1.3*
+
+## 패키지 그룹목록
+yum grouplist
+
+
+## "웹 서버" 그룹에 포함된 패키지 목록 출력
+yum groupinfo "웹 서버"
+
+## "웹 서버" 그룹 패키지 설치
+yum groupinstall "웹 서버"
+
+
+### yum 플러그인 검색
+yum search yum plugin
+
+### 체인지 로그와 다운로드 플러그인 설치
+yum install yum-plugin-changelog yum-plugin-downloadonly 
+
+## openssl 의 2014년 5월 이후의 변경 이력 확인
+yum changelog 2014-05 openssl
+
+## 패키지를 설치하지 않고 내려받기
+yum install postfix -y --downloadonly
+
+## 트랜잭션 목록 보기
+yum history list
+
+## 4번 트랜잭션의 자세한 이력 확인
+yum history info 4
+
+### 4번 트랜잭션 되돌리기
+yum history undo 4
+
+### 4번 트랜잭션 되살리기
+yum history redo 4
---
+++ chap06/6-1-selinux.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+## setuid 비트가 설정된 프로그램 검색
+find /bin /usr/bin /sbin -perm -4000 -exec ls -ldb {} \;
+
+## httpd 프로세스의 컨텍스트 확인
+ps -ZC httpd
+
+## 현재 동작 모드 확인
+getenforce
+
+## permissive 모드로 변경
+setenforce 0
+
+## SELinux 상태 출력
+sestatus
+
+## SELinux 의 모든 boolean 확인
+getsebool -a
+
+## 특정 boolean 확인
+getsebool httpd_can_sendmail 
+
+## boolean 값 설정(재부팅후 사라짐)
+setsebool httpd_can_sendmail true
+
+## boolean 값 영구 설정
+setsebool -P httpd_can_sendmail true
+
+## 파일의 컨텍스트 복구
+restorecon -R /var/www/html/test.html
+
+## 파일의 컨텍스트 설정
+chcon -R -t httpd_sys_content_t /var/www/html/test.html
+
+## 경로에 필요한 보안 컨텍스트 출력
+matchpathcon /var/log/ /var/www/ /etc/
+
+
+### semanage 패키지 설치
+yum install policycoreutils-python  
+
+## httpd 가 연결 가능한 포트 확인
+semanage port -l | grep http_port_t 
+
+## http 가 연결 가능한 포트 추가
+semanage port -a -p tcp -t http_port_t 800 
+
+## httpd_sys_content_t 가 할당된 파일 컨텍스트 확인
+semanage fcontext -l|grep httpd_sys_content_t
+
+## /data/www 디렉터리 아래는 httpd_sys_content_t 를 자동으로 부여
+semanage fcontext -a -t httpd_sys_content_t "/data/www(/.*)?"
+
+## audit.log 확인
+audit2why < /var/log/audit/audit.log   
+
+## setroubleshoot-server 패키지 설치 및 dbus 데몬 재구동
+yum install setroubleshoot-server 
+service messagebus restart
+
+## SELinux audit 로그 분석
+sealert -a /var/log/audit/audit.log
+
+## 로그파일에서 SELinux 관련 내용 추출
+grep "SELinux is preventing" /var/log/messages 
+grep "type=AVC" /var/log/audit/audit.log
+
+## aureport 로 로그 필터링(3월 10일 이후 audit 관련 로그만 출력)
+aureport -a -ts "2014년 3월 10일" 
+
+## 프로세스를 제한없이 실행되도록 설정
+chcon -t unconfined_exec_t /usr/sbin/httpd
